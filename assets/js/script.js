7ffc451e26e2b07bc20cdd287ccf6dcc9a591afc
Vanderlei Geronimo Junior <vand.9jr@gmail.com>
2024-11-17 08:26:46 -0300

    menu responsivo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,46 @@
+
+// CARRINHO DE COMPRAS
 let carrinho = document.querySelector(".carrinho")
 
 document.querySelector("#cart").onclick = () => {
+    // ABRE O CARRIONHO
     carrinho.classList.toggle("active")
 
+    // REMOVE O LOGIN
     login.classList.remove("active")
-
+    // REMOVE O MENU RESPONSIVO
+    menuResponsivo.classList.remove('active')
 }
 
+// LOGIN
 let login = document.querySelector(".login-form")
 
 document.querySelector("#login").onclick = () => {
+    // ABRE O LOGIN
     login.classList.toggle("active")
 
+    // REMOVE O CARRINHO
     carrinho.classList.remove("active")
+    // REMOVE O MENU RESPONSIVO
+    menuResponsivo.classList.remove('active')
+}
 
+// MENU RESPONSIVO
+let menuResponsivo = document.querySelector('.menu-site')
+document.querySelector('#menu').onclick = () => {
+    // ABRE O MENU RESPONSIVO
+    menuResponsivo.classList.toggle('active')
+
+    // REMOVE O LOGIN
+    login.classList.remove("active")
+    // REMOVE O CARRINHO
+    carrinho.classList.remove("active")
 }
 
 window.onscroll = () => {
     carrinho.classList.remove("active")
     login.classList.remove("active")
+    menuResponsivo.classList.remove("active")
 }
 
 // SLIDE PRINCIPAL
